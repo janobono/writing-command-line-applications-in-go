bc2e332027fa3964ff47a6de1e75da15112be68e
janobono <janobono@gmail.com>
2024-03-29 13:01:38 +0100

    CH02 challenge finished
    Signed-off-by: janobono <janobono@gmail.com>

--- ch02/challenge/rot13_test.go
+++ ch02/challenge/rot13_test.go
@@ -1,8 +1,61 @@
 package main
 
+import (
+	"github.com/stretchr/testify/require"
+	"os/exec"
+	"path"
+	"strings"
+	"testing"
+)
+
 /*
 Write tests that will execute rot13.
 - Test with text as argument
 	- Test with various inputs (table test, see t.Run)
 - Test with text from standard input
 */
+
+var rot13Cases = []struct {
+	text    string
+	encoded string
+}{
+	{"Cgo is not Go.", "Ptb vf abg Tb."},
+	{"Errors are values.", "Reebef ner inyhrf."},
+	{"Don't panic.", "Qba'g cnavp."},
+}
+
+func TestRot13(t *testing.T) {
+	exe := buildExe(t)
+
+	for _, tc := range rot13Cases {
+		t.Run(tc.text, func(t *testing.T) {
+			out, err := exec.Command(exe, tc.text).CombinedOutput()
+			require.NoError(t, err, "run")
+			encoded := strings.TrimSpace(string(out))
+			require.Equal(t, tc.encoded, encoded)
+		})
+	}
+}
+
+func TestRot13Stdin(t *testing.T) {
+	exe := buildExe(t)
+
+	for _, tc := range rot13Cases {
+		t.Run(tc.text, func(t *testing.T) {
+			cmd := exec.Command(exe)
+			cmd.Stdin = strings.NewReader(tc.text)
+			out, err := cmd.CombinedOutput()
+			require.NoError(t, err, "run")
+			encoded := strings.TrimSpace(string(out))
+			require.Equal(t, tc.encoded, encoded)
+		})
+	}
+}
+
+func buildExe(t *testing.T) string {
+	exe := path.Join(t.TempDir(), "logs")
+	output, err := exec.Command("go", "build", "-o", exe).CombinedOutput()
+	require.NoErrorf(t, err, "build:\n%s", string(output))
+
+	return exe
+}
